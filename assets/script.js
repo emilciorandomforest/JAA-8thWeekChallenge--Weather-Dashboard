bac60bf024897460bd223fd60656edeec8d130b5
Emil Cioran <john.a.alzate@gmail.com>
2024-01-12 18:59:59 -0500

    Add bootstrap stylesheet link to HTML, tinker with HTML queryURL on JS

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,20 @@
+// The variable declared below logs the developer's API Key
 const APIKey = "4c4e37224ec7f2e9b92313f00b179fce";
 
-const queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={${"APIKey"}}.";
+const queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=${`APIKey`}";
 
 
 // The code below creates Fetch request to OpenWeatherMap API
 // via the QueryURL variable declared above.
-fetch(queryURL)
+
+$("#search-button").on("click", function() {
+    fetch(queryURL)
     .then(function (response) {
-    return response.json();
+        return response.json();
     })
-.then(data)
+    .then(data)
     response(data)
+
+    const latLonURL = ""
+})
+
