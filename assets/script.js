ae9ace146fe5c377d8f36b676f33b42280f40249
Emil Cioran <john.a.alzate@gmail.com>
2024-01-12 18:44:37 -0500

    Create index.html file as well as CSS and JS files

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+const APIKey = "4c4e37224ec7f2e9b92313f00b179fce";
+
+const queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={${"APIKey"}}.";
+
+
+// The code below creates Fetch request to OpenWeatherMap API
+// via the QueryURL variable declared above.
+fetch(queryURL)
+    .then(function (response) {
+    return response.json();
+    })
+.then(data)
+    response(data)
