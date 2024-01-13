e9b526ecdd43ce16420eef528e4793ecd79b2643
Emil Cioran <john.a.alzate@gmail.com>
2024-01-12 19:48:53 -0500

    Add variables for lat and lon into fetch response

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,29 @@
 // The variable declared below logs the developer's API Key
 const APIKey = "4c4e37224ec7f2e9b92313f00b179fce";
 
-const queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=${`APIKey`}";
+const queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=$(`APIKey)";
 
-
+// 
+const input = $("#search-input").val().trim();
 // The code below creates Fetch request to OpenWeatherMap API
 // via the QueryURL variable declared above.
 
-$("#search-button").on("click", function() {
+$("#search-button").on("click", function(event) {
+    event.preventDefault();
+
     fetch(queryURL)
     .then(function (response) {
         return response.json();
     })
-    .then(data)
-    response(data)
+    .then(function (data) {
+        console.log(data)
+    })
 
-    const latLonURL = ""
+    const lat = ${.this}.data.coord.lat;
+    const lon = ${.this}.data.coord.lon;
+
+    const newQueryURL = "";
+
+
 })
 
